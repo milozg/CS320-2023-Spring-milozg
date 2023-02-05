8b16044e57d0db59cf84ad702deb2659908b1181
Milo Zurn-Galinsky <milozg@crc-dot1x-nat-10-239-1-127.bu.edu>
2023-02-05 15:39:37 -0500

    solved 01 working on 02

--- assigns/02/MySolution/assign02_01.py
+++ assigns/02/MySolution/assign02_01.py
@@ -8,7 +8,25 @@ print("[import ./../assign02.py] is done!")
 #
 # Please implement (20 points)
 # mylist_append (see list_append in assign02.sml)
+
+def mylist_append(xs,ys):
+    if mylist_nilq(xs):
+        return ys
+    else:
+        return mylist_cons(mylist_cons.get_cons1(xs),mylist_append(mylist_cons.get_cons2(xs),ys))
+
 # mylist_rappend (see list_rappend in assign02.sml)
+
+def mylist_rappend(xs,ys):
+    if mylist_nilq(xs):
+        return ys
+    else:
+        return mylist_rappend(mylist_cons.get_cons2(xs),mylist_cons(mylist_cons.get_cons1(xs),ys))
+
 # mylist_reverse (see list_reverse in assign02.sml)
+
+def mylist_reverse(xs):
+    return mylist_rappend(xs,mylist_nil())
+
 #
 ####################################################
--- assigns/02/MySolution/assign02_02.py
+++ assigns/02/MySolution/assign02_02.py
@@ -8,5 +8,27 @@ print("[import ./../assign02.py] is done!")
 #
 # Please implement (10 points)
 # mylist_quicksort (see list_quicksort in assign02.sml)
+
+def mylist_qpart(xs,n):
+    if mylist_nilq(xs):
+        return (mylist_nil(),mylist_nil())
+    else:
+        (ys,zs) = mylist_qpart(xs,n)
+        x1 = mylist_cons.get_cons1(xs)
+        if x1 <= n:
+            return (mylist_cons(x1,ys),zs)
+        else:
+            return (ys,mylist_cons(x1,zs))
+
+def mylist_quicksort(xs):
+    if mylist_nilq(xs):
+        return mylist_nil()
+    else:
+        x1 = mylist_cons.get_cons1(xs)
+        (ys,zs) = mylist_qpart(xs,x1)
+        ys = mylist_quicksort(ys)
+        zs = mylist_quicksort(zs)
+        return mylistcons(ys,mylist_cons(x1,zs))
+
 #
 ####################################################

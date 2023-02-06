9377275522dee117ddf3891f70a928910a100610
Milo Zurn-Galinsky <milozg@crc-dot1x-nat-10-239-1-127.bu.edu>
2023-02-06 16:58:41 -0500

    nested helper functions

--- assigns/02/MySolution/assign02_02.py
+++ assigns/02/MySolution/assign02_02.py
@@ -10,18 +10,19 @@ print("[import ./../assign02.py] is done!")
 # Please implement (10 points)
 # mylist_quicksort (see list_quicksort in assign02.sml)
 
-def mylist_qpart(xs,n):
-    if mylist_nilq(xs):
-        return (mylist_nil(),mylist_nil())
-    else:
-        (ys,zs) = mylist_qpart(mylist_cons.get_cons2(xs),n)
-        x1 = mylist_cons.get_cons1(xs)
-        if x1 <= n:
-            return (mylist_cons(x1,ys),zs)
-        else:
-            return (ys,mylist_cons(x1,zs))
 
 def mylist_quicksort(xs):
+    def mylist_qpart(xs,n):
+        if mylist_nilq(xs):
+            return (mylist_nil(),mylist_nil())
+        else:
+            (ys,zs) = mylist_qpart(mylist_cons.get_cons2(xs),n)
+            x1 = mylist_cons.get_cons1(xs)
+            if x1 <= n:
+                return (mylist_cons(x1,ys),zs)
+            else:
+                return (ys,mylist_cons(x1,zs))
+
     if mylist_nilq(xs):
         return mylist_nil()
     else:

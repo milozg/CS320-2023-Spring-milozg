3156c6fb7d6c67abf972c03dc2ef8bca1a5e988e
Milo Zurn-Galinsky <milozg@crc-dot1x-nat-10-239-214-189.bu.edu>
2023-03-20 23:56:43 -0400

    solved 02

--- assigns/05/MySolution/Python/assign05_02.py
+++ assigns/05/MySolution/Python/assign05_02.py
@@ -10,6 +10,22 @@ from mypylib_cls import *
 # Please *translate* into Python the posted solution
 # on Piazza for word_neighbors (which is writtend in SML)
 #
+def strsub(word, i):
+    return word[i]
+
+def string_length(word):
+    return string_foldleft(word, 0, lambda r,_: r + 1)
+
+def string_implode(ss):
+    return foreach_to_foldleft(fnlist_foreach)(ss, "", lambda r,s: r + s)
+
+def fnlist_tabulate(n,f):
+    return fnlist_reverse\
+            (int1_foldleft(n,fnlist_nil(),lambda xs,i: fnlist_cons(f(i),xs)))
+
+def string_tabulate(n,f):
+    return string_implode(fnlist_tabulate(n,f))
+
 def word_neighbors(word):
     """
     Note that this function should returns a fnlist
@@ -17,6 +33,14 @@ def word_neighbors(word):
     Your implementation should be combinator-based very
     much like the posted solution.
     """
-    raise NotImplementedError
+    wlen = string_length(word)
+    AB = "abcdefghijklmnopqrstuvwxyz"
+
+    return \
+        fnlist_concat(string_imap_fnlist\
+                      (word, lambda i, c: fnlist_concat\
+                       (string_imap_fnlist(AB, lambda _, c1: fnlist_sing\
+                                           (string_tabulate(wlen, lambda j: strsub(word, j) if i != j else c1)) if (c != c1) else fnlist_nil()))))
+
 #
 ####################################################

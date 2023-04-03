3eef99cb13d9e0c68837e82d4f7894769c2117e4
Milo Zurn-Galinsky <milozg@crc-dot1x-nat-10-239-233-121.bu.edu>
2023-04-03 13:28:30 -0400

    merge

--- quizzes/part-03-30/part_03_30.py
+++ quizzes/part-03-30/part_03_30.py
@@ -54,7 +54,7 @@ def image_vreflect(image):
     return \
         imgvec.image_make_i2map\
         (image, \
-         lambda i0, j0, v0: imgvec.image_get_pixel(image, (i0+hh)%hh, j0))
+         lambda i0, j0, v0: imgvec.image_get_pixel(image, i0//(hh-i0), j0))
 ####################################################
 balloons = \
     load_color_image\
